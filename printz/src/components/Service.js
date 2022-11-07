4d455fe196606a728999a9457a4e5c82d0080381
hitallof <hitalloufpe@gmail.com>
2022-11-07 08:39:15 -0300

    txt

--- printz/src/components/Service.js
+++ printz/src/components/Service.js
@@ -29,44 +29,24 @@ const Service = () => {
         </ul>
 
         <h2>Creates Unique and Authentic Outputs</h2>
-Screen printing can create a different visual output when compared to digital<br/>  printing. Screen printing ink is typically applied more thickly, resulting in longer-lasting,<br/>  brighter and more vibrant colours, even on darker backgrounds. For garment design<br/>  that requires a high level of vibrancy when printing on dark grounds, screen printing is <br/> still the best option
-    
-    
-        </div>
-        <div className={styles.silkimg}>
-        <h2>Cost-Efficient & Yields Large Volumes</h2>
-    Screen printing can create a different visual output when comhpared to digital <br/>printing. Screen printing ink is typically applied more thickly, resulting in <br/>longer-lasting, brighter and more vibrant colours, even on<br/> darker backgrounds. For garment design that requires a high<br/> level of vibrancy when printing on dark grounds, <br/>screen printing is still the best option.
-    <h2>Very Versatile</h2>
-    Screen printing accommodates a wide range of materials, including vinyl, fabric,<br/>  rigid plastic, foam board, metal, glass and many others. The printer can produce <br/> different products, such as banners, T-shirts, hats, posters and wallpapers using the same screens. <br/> It is also not limited by size or shape, so distinctive shapes like<br/>  those of mugs and bottles can be designed using this printing method.
-      
-      <h2>Provides Long-lasting, High Quality Results</h2>
-      <p>The ability of the screen printing ink, like water based inks, to bond to the material of<br/> the garment can result in a much more durable graphic compared to digital printing.<br/> Heat cured screen prints are durable enough to handle repeat <br/>washing without cracking or fading.</p>
-      
-        </div>
-        </div>
+<p>Screen printing can create a different visual output when compared to digital<br/>  printing. Screen printing ink is typically applied more thickly, resulting in longer-lasting,<br/>  brighter and more vibrant colours, even on darker backgrounds. For garment design<br/>  that requires a high level of vibrancy when printing on dark grounds, screen printing is <br/> still the best option</p>
+    </div>
+  </div>     
       </div>
-      <div className={styles.bordado}>
-      <div className={styles.bordadoimg}>
-          <img src={Bordado}></img>
-        </div>
-        <div className={styles.bordadotxt}>
-        <h1>BORDADO</h1>
-        <p>O silk screen, também chamado de serigrafia, é um processo de impressão.<br/> Nessa técnica, que pode ser realizada por uma pessoa, de forma mecânica,<br/>  ou automaticamente, por máquinas, a tinta é vazada, por meio da pressão de um rodo. Esse rodo é passado ao longo de um tela<br/>  (feito de nylon ou seda), que é esticada em um bastidor de madeira ou de aço.</p>
-        </div>
+      <div>
+      <h2>HEAT PRESS</h2>
+      <div>
+        <p>A heat press is used to permanently apply a heat transfer to a surface. Common transfer types are Heat Transfer Vinyl cut with a vinyl cutter, Printable Heat Transfer Vinyl, Inkjet Transfer Paper, Laser Transfer Paper, Plastisol Transfers, and Sublimation.</p>
+      </div>
+      <div>
+        <p>HTV works best on cotton or polyester or cotton/poly blends. Other synthetic fabrics, like acrylic, won't work correctly because they will melt under the heat of an iron. For more advanced crafters, you can apply HTV to mugs, baseball hats, footballs and even wood!</p>
+      </div>
+
+
 
       </div>
-      <div className={styles.heat}>
-        <div className={styles.heattxt}>
-        <h1>HEAT PRESS</h1>
-        <p>O silk screen, também chamado de serigrafia, é um processo de impressão.<br/> Nessa técnica, que pode ser realizada por uma pessoa, de forma mecânica,<br/>  ou automaticamente, por máquinas, a tinta é vazada, por meio da pressão de um rodo. Esse rodo é passado ao longo de um tela<br/>  (feito de nylon ou seda), que é esticada em um bastidor de madeira ou de aço.</p>
         </div>
-        <div className={styles.heat}>
-          <img src={Heat}></img>
-        </div>
-      </div>
-      <div className={styles.silk}></div>
-      <div></div>
-      </div>
+
 </section>
   )
 }
